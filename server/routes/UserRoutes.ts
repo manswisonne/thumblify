c9cc4ba29f8c1bf2b21c0d1a915b3a1c4714dd8d
Manaswi Sonne <manswisonne151@gmail.com>
2026-02-12 13:50:05 +0530

    Fix import statement for protect middleware

--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserThumbnails, getThumbnailById } from "../controllers/UserController.js";
-import protect from "../middlewares/auth.js";
+import {protect }from "../middlewares/auth.js";
 const UserRouter = express.Router();
 UserRouter.get('/thumbnails',protect,getUserThumbnails);
 UserRouter.get('/thumbnail/:id',protect,getThumbnailById);
