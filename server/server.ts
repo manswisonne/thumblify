28c49b84bfdd766fb1319f277cde4615e7487c87
Manaswi Sonne <manswisonne151@gmail.com>
2026-02-12 13:20:58 +0530

    Update cookie settings for production
    Set secure cookies to true and changed sameSite to 'none'.

--- server/server.ts
+++ server/server.ts
@@ -398,8 +398,8 @@ app.use(session({
     cookie: { 
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: true,
+        sameSite:'none',
         path: '/',
     },
     store: MongoStore.create({
