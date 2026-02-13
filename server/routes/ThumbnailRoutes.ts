39d4c0d896760cc4bde934fe1b57e97800ae03aa
Manaswi Sonne <manswisonne151@gmail.com>
2026-02-13 12:53:44 +0530

    Update ThumbnailRoutes.ts

--- server/routes/ThumbnailRoutes.ts
+++ server/routes/ThumbnailRoutes.ts
@@ -1,8 +1,8 @@
 import Express from "express";
 import { deleteThumbnail, generateThumbnail } from "../controllers/ThumbnailController.js";
-import {protect} from "../middlewares/auth.js";
+import {auth} from "../middlewares/auth.js";
 const ThumbnailRouter = Express.Router();
-ThumbnailRouter.post('/generate', auth,generateThumbnail)
-ThumbnailRouter.delete('/delete/:id', auth,deleteThumbnail)
+ThumbnailRouter.post('/generate', auth ,generateThumbnail)
+ThumbnailRouter.delete('/delete/:id', auth ,deleteThumbnail)
 
 export default ThumbnailRouter;
